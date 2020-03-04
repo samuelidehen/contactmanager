0f844191a6825f5dc403cc3a9133766ed7e38fe9
Samuel Idehen <samuelidehenlion@gmail.com>
2020-03-04 01:11:49 -0500

    Updates

---
+++ precache-manifest.d6c9dd06aabb78e559bc4d3ec4d83fb4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a45a5abad6a83aba5af2305e0ce156c5",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "6b661e5f2a98db7ac0c4",
+    "url": "/contactmanager/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "0c3db88a63cd2218dc50",
+    "url": "/contactmanager/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "6b661e5f2a98db7ac0c4",
+    "url": "/contactmanager/static/js/2.cc36eb45.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/contactmanager/static/js/2.cc36eb45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c3db88a63cd2218dc50",
+    "url": "/contactmanager/static/js/main.1e275c1d.chunk.js"
+  },
+  {
+    "revision": "22a1eb616042739cf5e8",
+    "url": "/contactmanager/static/js/runtime-main.900c19c8.js"
+  }
+]);
