23a8106027eb84d5cfbf41bcbb653195f56d4dc1
Samuel Idehen <samuelidehenlion@gmail.com>
2020-05-03 21:36:55 -0400

    ---labeling

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,6 +6,7 @@ export default function About() {
       <h1 className="display-4">About Contact Manager</h1>
       <p className="lead">Simple app to manage Contacts</p>
       <p className="text-secondary">version 1.0.1</p>
+      <p className="text-secondary">created by Samuel Idehen</p>
     </div>
   );
 }
--- src/components/pages/NotFoundPage.js
+++ src/components/pages/NotFoundPage.js
@@ -6,7 +6,7 @@ export default function NotFoundPage() {
       <h1 className="display-4">
         <span className="text-danger">404</span> page Not Found{" "}
       </h1>
-      <p className="lead">The page does not exist </p>
+      <p className="lead">The page does not exist or is unavailable</p>
     </div>
   );
 }
